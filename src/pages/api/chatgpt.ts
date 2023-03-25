391447e84b5e249b6332afdfe1efdfe6fc45bfd9
satoshi02198 <awareness_yoga@yahoo.co.jp>
2023-03-26 02:31:25 +0900

    20230326

--- src/pages/api/chatgpt.ts
+++ src/pages/api/chatgpt.ts
@@ -1,5 +1,6 @@
 import fetch from 'node-fetch';
 import type { NextApiRequest, NextApiResponse } from 'next';
+import { fetchWithRetry } from '../../../utils/fetchWithRetry';
 
 interface Message {
   role: string;
@@ -20,17 +21,20 @@ export default async function handler(
   res: NextApiResponse
 ) {
   try {
-    const response = await fetch('https://api.openai.com/v1/chat/completions', {
-      method: 'POST',
-      headers: {
-        Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        model: 'gpt-3.5-turbo',
-        messages: req.body.messages,
-      }),
-    });
+    const response = await fetchWithRetry(
+      'https://api.openai.com/v1/chat/completions',
+      {
+        method: 'POST',
+        headers: {
+          Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          model: 'gpt-3.5-turbo',
+          messages: req.body.messages,
+        }),
+      }
+    );
 
     const { choices, error } = (await response.json()) as OpenAIResponse;
 
